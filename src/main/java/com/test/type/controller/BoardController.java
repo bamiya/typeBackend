94651d97d65ad95533bcba6d0c7e95e02ffa78e3
bamiyamy <bamiyamy@gmail.com>
2023-11-03 16:43:14 +0900

    board detail controller 추가

--- src/main/java/com/test/type/controller/BoardController.java
+++ src/main/java/com/test/type/controller/BoardController.java
@@ -1,19 +1,29 @@
 package com.test.type.controller;
 
 import com.test.type.dto.BoardDTO;
+import com.test.type.entity.BoardEntity;
 import com.test.type.service.BoardService;
+import com.test.type.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/board")
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class BoardController {
     private final BoardService boardService;
+    private final BoardRepository boardRepository;
+
+
+    public BoardController(BoardService boardService, BoardRepository boardRepository) {
+        this.boardService = boardService;
+        this.boardRepository = boardRepository;
+    }
 
     @GetMapping
     public ResponseEntity<List<BoardDTO>> getAllPosts() {
@@ -25,6 +35,14 @@ public class BoardController {
     public ResponseEntity<Void> createPost(@RequestBody BoardDTO boardDTO) {
         boardService.save(boardDTO);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BoardEntity> getBoardDetail(@PathVariable Long id) {
+        Optional<BoardEntity> board = boardRepository.findById(id);
+        return board.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PutMapping("/{id}")
--- src/main/java/com/test/type/repository/BoardRepository.java
+++ src/main/java/com/test/type/repository/BoardRepository.java
@@ -3,5 +3,8 @@ package com.test.type.repository;
 import com.test.type.entity.BoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
+    Optional<BoardEntity> findById(Long id);
 }
